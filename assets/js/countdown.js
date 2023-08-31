e5894e20ad74cf4a7808fff9429875a6305e6e19
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2023-08-31 08:20:06 +0530

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const changingText = document.querySelector('.changing-text span');
     const countdownElement = document.getElementById('countdown');
-    const endDate = new Date('2023-08-31T09:00:00').getTime();
+    const endDate = new Date('2023-08-31T08:45:00').getTime();
 
     function updateChangingText() {
         const texts = ['mastery.', 'brilliance.', 'proficiency.'];
